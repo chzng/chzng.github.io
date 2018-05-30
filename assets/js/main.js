456d928a6f67eaec6105072f9252b811fdcc0aa7
Chzng <chzng@users.noreply.github.com>
2018-05-30 22:29:46 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,21 @@ $(document).ready(function() {
 
   // Menu Settings
   $('.menu-icon, .menu-icon-close').click(function(e) {
-    //e.preventDefault();
+    e.preventDefault();
     $('.flex-container').toggleClass('active');
-    $('.flex-container.active .page-mask').on('click',function(){
-      $('.flex-container').removeClass('active');
-    });
+    
+    // Mask Click
+    if('.flex-container').hasClass('active') {
+      $('.flex-container.active .page-mask').on('click',function(){
+        $('.flex-container').removeClass('active');
+      });
+    }
   });
   
   // Mask Click
-  $('.flex-container.active .page-mask').on('click',function(){
-    $('.flex-container').removeClass('active');
-  });
+//   $('.flex-container.active .page-mask').on('click',function(){
+//     $('.flex-container').removeClass('active');
+//   });
   
   // Search Settings
   $('.search-icon').on('click', function(e){
